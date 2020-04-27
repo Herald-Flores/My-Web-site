4a7ab3935637653bef4405a62cd75a31af437969
Herald Flores <herald.flores@boombit.agency>
2020-04-26 21:14:43 -0600

    Avance en one page

--- dev/assets/js/imports.js
+++ dev/assets/js/imports.js
@@ -42,6 +42,50 @@ $('.navbar-toggler').click(function () {
     $('html, body').toggleClass('not-scroll');
 });
 
+var header = document.getElementById("navbarNavDropdown");
+var btns = header.getElementsByClassName("nav-item");
+for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+
+
+
+//progress bard
+
+
+function animateElements() {
+    $('.progressbar').each(function () {
+        var elementPos = $(this).offset().top;
+        var topOfWindow = $(window).scrollTop();
+        var percent = $(this).find('.circle').attr('data-percent');
+        var animate = $(this).data('animate');
+        if (elementPos < topOfWindow + $(window).height() - 30 && !animate) {
+            $(this).data('animate', true);
+            $(this).find('.circle').circleProgress({
+                // startAngle: -Math.PI / 2,
+                value: percent / 100,
+                size : 400,
+                thickness: 15,
+                fill: {
+                    color: '#663399'
+                }
+            }).on('circle-animation-progress', function (event, progress, stepValue) {
+                $(this).find('strong').text((stepValue*100).toFixed(0) + "%");
+            }).stop();
+        }
+    });
+}
+
+animateElements();
+$(window).scroll(animateElements);
+
+
+
+
 // World countries array 
 // { name, topLevelDomain, alpha2Code, alpha3Code, callingCodes, capital, altSpellings, region, subregion
 // population, latlng, demonym, area, gini, timezones, borders, nativeName, numericCode, currencies, languages
